f6c4eef31f6f61faa8cb3b993bd73da60a0fa9d9
work <danil.chest@gmail.com>
2018-11-30 01:18:37 +0500

    added blacklist functionality

--- src/dnsproxy.py
+++ src/dnsproxy.py
@@ -6,38 +6,32 @@ import configparser
 from datetime import datetime
 from time import sleep
 
-from dnslib import QTYPE, RR, dns
+from dnslib import RR, QTYPE, A, dns, DNSHeader, DNSQuestion
 from dnslib.proxy import ProxyResolver
 from dnslib.server import DNSServer, DNSRecord
 
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s', datefmt='%H:%M:%S'))
-
-logger = logging.getLogger(__name__)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
-
 class Resolver(ProxyResolver):
-    def __init__(self, dns_server):
-        super().__init__(dns_server, 53, 5)
+    def __init__(self, address, blacklist):
+        self.blacklist = blacklist.split(", ")
+        self.response = config['Proxy']['response']
+        super().__init__(address, 53, 5)
 
     def resolve(self, request, handler):
-        proxy_r = request.send(self.address,self.port,
-                                timeout=self.timeout)
-        type_name = QTYPE[request.q.qtype]
-        #reply = request.reply()
-        reply = DNSRecord.parse(proxy_r)
-        print (reply)
-        return super().resolve(request, handler)
-    
-    def blacklisted(s, blacklist):
-        result=[]
-        result = blacklist.split(", ")
-        if s in result:
-            return config['Proxy']['response']
+        qname = request.q.qname
+        
+        if (self.blacklisted(qname, self.blacklist)):
+            reply = request.reply()
+            reply.add_answer(RR(qname,QTYPE.A,ttl=300,rdata=A(self.response)))
+            print(reply)
+            return reply
         else:
-            return 0
+            return super().resolve(request, handler)
+
+    def blacklisted(self, domain, blacklist):
+        if domain in blacklist:
+            return self.response
+        else:
+            return ""
 
 def handle_sig(signum, frame):
     exit(0)
@@ -50,12 +44,12 @@ if __name__ == '__main__':
     
     port = int(config['Proxy']['port'])
     dns_server = config['DNS']['ip']
-
-    resolver = Resolver(dns_server)
+    blacklist = config['Proxy']['blacklist']
+    resolver = Resolver(dns_server, blacklist)
     proxy_server = DNSServer(resolver, port=port)
 
 
-    logger.info('starting DNS server on port %d, upstream DNS server "%s"', port, dns_server)
+    print('starting DNS server on port ' + str(port) + ', upstream DNS server \"' + dns_server + '"')
     proxy_server.start_thread()
 
     try:
